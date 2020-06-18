339bfc43568067db6ab1a151276b50fe2163d757
Anna <xsmorex@users.noreply.github.com>
2020-06-18 12:41:24 +0100

    Update emailerTest.js

--- src/emailerTest.js
+++ src/emailerTest.js
@@ -1,6 +1,6 @@
 const Emailer = require("./Emailer");
 
-let email = new Emailer("banasikannie@gmail.com", "some hash", "https://github.com/xsmorex");
+let email = new Emailer("", "some hash", "https://github.com/xsmorex");
 
 let emailSendCallback = function(err, info) {
   console.log(err);
